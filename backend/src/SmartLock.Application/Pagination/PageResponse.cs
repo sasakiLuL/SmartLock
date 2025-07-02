81613266b6b15b34f865bfef30451eb118ea9358
Sasha Kryvko <sashakrivko75@gmail.com>
2025-07-02 11:58:26 +0200

    Added pagination service and response

---
+++ backend/src/SmartLock.Application/Pagination/PageResponse.cs
@@ -0,0 +1,44 @@
+﻿namespace SmartLock.Application.Pagination;
+
+public record PageResponse<TItem>
+{
+    public int PageSize { get; init; }
+
+    public int PageNumber { get; init; }
+
+    public int PageCount { get; init; }
+
+    public int TotalItemCount { get; init; }
+
+    public List<TItem> PageItems { get; init; }
+
+    private PageResponse(
+        int pageSize,
+        int pageNumber,
+        int pageCount,
+        int totalItemCount,
+        List<TItem> pageItems) 
+    {
+        PageCount = pageCount;
+        PageSize = pageSize;
+        PageNumber = pageNumber;
+        TotalItemCount = totalItemCount;
+        PageItems = pageItems;
+    }
+
+    public static PageResponse<TItem> Create(
+        List<TItem> items,
+        int totalItemCount,
+        int pageNumber, 
+        int pageSize)
+    {
+        int count = totalItemCount;
+
+        return new PageResponse<TItem>(
+            pageSize,
+            pageNumber,
+            count % pageSize == 0 ? count / pageSize : count / pageSize + 1,
+            count,
+            items);
+    }
+}
---
+++ backend/src/SmartLock.Application/Pagination/PaginationService.cs
@@ -0,0 +1,16 @@
+﻿namespace SmartLock.Application.Pagination;
+
+public class PaginationService
+{
+    public IQueryable<TItem> GetPagedQuery<TItem>(
+        IQueryable<TItem> query,
+        int pageNumber,
+        int pageSize)
+    {
+        var pagedQuery = query
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize);
+
+        return pagedQuery;
+    }
+}

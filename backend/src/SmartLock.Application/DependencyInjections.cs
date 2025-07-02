ff7665911303eb8b7ed09455146761348098ea24
Sasha Kryvko <sashakrivko75@gmail.com>
2025-07-02 12:03:22 +0200

    Configured messaging. Added references and packages

--- backend/src/SmartLock.Application/DependencyInjections.cs
+++ backend/src/SmartLock.Application/DependencyInjections.cs
@@ -1,9 +1,14 @@
 ﻿using FluentValidation;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using SmartLock.Application.Abstractions.Messages;
-using SmartLock.Application.Devices.ConfirmActivation;
-using SmartLock.Application.Devices.Deactivate;
+using SmartLock.Application.Features.Devices.Activate;
+using SmartLock.Application.Features.Devices.Deactivate;
+using SmartLock.Application.Features.Devices.RejectedMessage;
+using SmartLock.Application.Features.Devices.StateReceived;
+using SmartLock.Application.Features.Devices.Toggle;
+using SmartLock.Application.Options;
+using SmartLock.Application.Shadows;
+using SmartLock.Messaging.Extensions;
 using System.Reflection;
 
 namespace SmartLock.Application;
@@ -12,14 +17,31 @@ public static class DependencyInjections
 {
     public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddMediatR(config => 
+        services.AddMediatR(config =>
             config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
 
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
-        services.AddScoped<IDeviceMessageConsumerHandler, ActivationResponseMessageConsumerHandler>();
+        MqttOptions mqttOptions = configuration.GetSection(MqttOptions.Section).Get<MqttOptions>()
+            ?? throw new InvalidDataException();
 
-        services.AddScoped<IDeviceMessageConsumerHandler, DeactivateRequestMessageHandler>();
+        services.AddMessaging(conf =>
+        {
+            conf.Host((options) =>
+            {
+                options.Host = mqttOptions.Endpoint;
+                options.Port = mqttOptions.Port;
+                options.ClientId = mqttOptions.ClientId;
+                options.CertificatePath = mqttOptions.CertificatePath;
+                options.RootCAPath = mqttOptions.RootCAPath;
+                options.PrivateKeyPath = mqttOptions.PrivateKeyPath;
+            });
+
+            conf.ConsumerTopic<MissingMessageConsumer, ShadowErrorMessage>(mqttOptions.ConsumerPolicies["UpdateRejected"]);
+            conf.ConsumerTopic<StateReceivedMessageConsumer, Shadow>(mqttOptions.ConsumerPolicies["UpdateAccepted"]);
+
+            conf.MapProducerTopic<Shadow>(mqttOptions.ProducerPolicies["Update"]);
+        });
 
         return services;
     }

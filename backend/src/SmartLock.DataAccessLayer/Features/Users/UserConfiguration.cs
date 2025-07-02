298d304e8763dbc59c74664509e439da1d2dba6a
Sasha Kryvko <sashakrivko75@gmail.com>
2025-07-02 12:16:39 +0200

    Refactored user repository and configuration

---
+++ backend/src/SmartLock.DataAccessLayer/Features/Users/UserConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmartLock.Domain.Features.Users;
+
+namespace SmartLock.DataAccessLayer.Features.Users;
+
+public class UserConfiguration : IEntityTypeConfiguration<UserModel>
+{
+    public void Configure(EntityTypeBuilder<UserModel> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.HasIndex(x => x.Id);
+
+        builder.Property(x => x.IdentityProviderId)
+            .IsRequired();
+
+        builder.ComplexProperty(
+            x => x.Email, 
+            builder =>
+            {
+                builder.Property(x => x.Value)
+                    .IsRequired();
+            });
+
+        builder.ComplexProperty(
+            x => x.Username,
+            builder =>
+            {
+                builder.Property(x => x.Value)
+                    .IsRequired();
+            });
+    }
+}
---
+++ backend/src/SmartLock.DataAccessLayer/Features/Users/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartLock.Domain.Features.Users;
+using SmartLock.Domain.ValueObjects.EmailAddresses;
+using SmartLock.Domain.ValueObjects.Usernames;
+
+namespace SmartLock.DataAccessLayer.Features.Users;
+
+public class UserRepository(SmartLockContext smartLockContext) : Repository<User, UserModel>(smartLockContext), IUserRepository
+{
+    public async Task<bool> IsEmailUniqueAsync(EmailAddress email, CancellationToken cancellationToken = default)
+    {
+        return await _context.Users
+            .AllAsync(x => x.Email != email, cancellationToken);
+    }
+
+    public async Task<bool> IsUsernameUniqueAsync(Username username, CancellationToken cancellationToken = default)
+    {
+        return await _context.Users
+            .AllAsync(x => x.Username != username, cancellationToken);
+    }
+
+    public async Task<User?> ReadByIdAsync(Guid userId, CancellationToken cancellationToken = default)
+    {
+        var user = await _context.Users
+            .FirstOrDefaultAsync(x => x.Id == userId, cancellationToken);
+
+        return user is not null ? new User(user) : null;
+    }
+
+    public async Task<User?> ReadByIdentityProviderIdAsync(Guid identityProviderId, CancellationToken cancellationToken = default)
+    {
+        var user = await _context.Users
+            .FirstOrDefaultAsync(x => x.IdentityProviderId == identityProviderId, cancellationToken);
+
+        return user is not null ? new User(user) : null;
+    }
+}

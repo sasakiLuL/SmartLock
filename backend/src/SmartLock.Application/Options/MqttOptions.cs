3e25a46e3ecd46e134a29a7d07d58128d668c371
Sasha Kryvko <sashakrivko75@gmail.com>
2025-07-02 11:57:10 +0200

    Added mqtt options

---
+++ backend/src/SmartLock.Application/Options/MqttOptions.cs
@@ -0,0 +1,22 @@
+﻿namespace SmartLock.Application.Options;
+
+public class MqttOptions
+{
+    public static readonly string Section = "DeviceMessageBroker";
+
+    public string ClientId { get; set; } = string.Empty;
+
+    public string Endpoint { get; set; } = string.Empty;
+
+    public int Port { get; set; }
+
+    public Dictionary<string, string> ConsumerPolicies { get; set; } = [];
+
+    public Dictionary<string, string> ProducerPolicies { get; set; } = [];
+
+    public string CertificatePath { get; set; } = string.Empty;
+
+    public string PrivateKeyPath { get; set; } = string.Empty;
+
+    public string RootCAPath { get; set; } = string.Empty;
+}

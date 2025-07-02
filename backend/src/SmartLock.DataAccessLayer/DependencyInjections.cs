ddb1bfe60c44196104ad4ee285eddd04cb9eb6e8
Sasha Kryvko <sashakrivko75@gmail.com>
2025-07-02 12:19:48 +0200

    Added interector and action repository in DI

--- backend/src/SmartLock.DataAccessLayer/DependencyInjections.cs
+++ backend/src/SmartLock.DataAccessLayer/DependencyInjections.cs
@@ -1,13 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using SmartLock.Application.Abstractions;
-using SmartLock.DataAccessLayer.Actions;
-using SmartLock.DataAccessLayer.Locks;
-using SmartLock.DataAccessLayer.Users;
-using SmartLock.Domain.Actions;
-using SmartLock.Domain.Devices;
-using SmartLock.Domain.Users;
+using Microsoft.Extensions.Logging;
+using SmartLock.Application.Interfaces;
+using SmartLock.DataAccessLayer.Features.Devices;
+using SmartLock.DataAccessLayer.Features.Users;
+using SmartLock.DataAccessLayer.Interceptors;
+using SmartLock.Domain.Features.Devices;
+using SmartLock.Domain.Features.Users;
 
 namespace SmartLock.DataAccessLayer;
 
@@ -15,19 +15,26 @@ public static class DependencyInjections
 {
     public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
     {
-        var connectionString = configuration.GetConnectionString("Database") ??
-            throw new ArgumentNullException("Connection string is not exist");
+        var connectionString = configuration.GetConnectionString("Database")
+            ?? throw new ArgumentNullException(nameof(configuration), "Connection string is not exist");
 
-        services.AddDbContext<SmartLockContext>(options => 
-            options.UseNpgsql(
-                connectionString, 
-                options => options.EnableRetryOnFailure()));
+        services.AddScoped<PublishDomainEventsInterceptor>();
+
+        services.AddDbContext<SmartLockContext>((serviceProvider, options) =>
+        {
+            var interceptor = serviceProvider.GetRequiredService<PublishDomainEventsInterceptor>();
+            options
+                .UseNpgsql(
+                    connectionString,
+                    options => options.EnableRetryOnFailure())
+                .AddInterceptors([interceptor]);
+        });
+
+        services.AddScoped<IReadModelService, ReadModelService>();
 
         services.AddScoped<IUserRepository, UserRepository>();
 
         services.AddScoped<IDeviceRepository, DeviceRepository>();
-
-        services.AddScoped<IActionRepository, ActionRepository>();
 
         services.AddScoped<IUnitOfWork, UnitOfWork>();
 
